8a2aeecfc37f5b79d3cd3c486c6d566f29b1dd2a
Muskan Kumari <mkr803967@gmail.com>
2026-06-24 22:49:08 +0530

    Update page.tsx

--- IPL-Team-Website-CSKVerse/app/page.tsx
+++ IPL-Team-Website-CSKVerse/app/page.tsx
@@ -1,13 +1,13 @@
-import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
-import TeamOverview from "@/components/TeamOverview";
-import Squad from "@/components/Squad";
-import MatchSchedule from "@/components/MatchSchedule";
-import Stats from "@/components/Stats";
-import FanZone from "@/components/FanZone";
-import News from "@/components/News";
-import Footer from "@/components/Footer";
-import BackToTop from "@/components/BackToTop";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import TeamOverview from "../components/TeamOverview";
+import Squad from "../components/Squad";
+import MatchSchedule from "../components/MatchSchedule";
+import Stats from "../components/Stats";
+import FanZone from "../components/FanZone";
+import News from "../components/News";
+import Footer from "../components/Footer";
+import BackToTop from "../components/BackToTop";
 
 export default function Home() {
   return (
